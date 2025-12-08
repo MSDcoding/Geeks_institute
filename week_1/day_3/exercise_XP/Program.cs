68f3cb95c92668146a9901f1e3817ee0c55b2c9d
MSDCoding <moussaouidriss052@gmail.com>
2025-12-08 11:18:23 +0100

    dayy3 exr6

---
+++ week_1/day_3/exercise_XP/Program.cs
@@ -0,0 +1,14 @@
+﻿ 
+using System;
+
+class Program
+{
+	static void Main()
+	{
+		Exercise1.ConvertList();
+		Exercise2.cinemax();
+		Exercise4.cities("rabat","morocco");
+	    Exercise5.NumberGuess(19);
+	    Exercise6.personaliz();
+	}
+}
