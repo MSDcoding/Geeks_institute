fa667e172f8fe91e6825972996aa859cf086993b
MSDcoding <drissmoussaoui58@gmail.com>
2026-01-24 17:28:02 +0100

    console app

---
+++ week_1/day_3/Daily-Challenge/console_application/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter the word : ");
+        string word = Console.ReadLine();
+
+        Dictionary<char, List<int>> letterPositions = new Dictionary<char, List<int>>();
+
+        for (int i = 0; i < word.Length; i++)
+        {
+            char letter = word[i];
+
+            if (!letterPositions.ContainsKey(letter))
+            {
+                letterPositions[letter] = new List<int>();
+            }
+
+            letterPositions[letter].Add(i);
+        }
+
+        Console.WriteLine("\n resultat :");
+        foreach (var item in letterPositions)
+        {
+            Console.Write(item.Key + " : [ ");
+            foreach (int index in item.Value)
+            {
+                Console.Write(index + " ");
+            }
+            Console.WriteLine("]");
+        }
+    }
+}
